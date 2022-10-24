c2add62f9c3cba9e97bbee8489230d9588459600
Victor Inojosa <vijoin@gmail.com>
2022-10-23 21:18:17 -0300

    Change blog title

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Victor Inojosa';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = "vijoin's Blog!";
 
 export default function Layout({ children, home }) {
   return (
