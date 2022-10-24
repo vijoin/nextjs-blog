e57716534542f7344752d1bdc09627b70b34d464
Victor Inojosa <vijoin@gmail.com>
2022-10-24 10:11:33 -0300

    Open RRSS links in a new tab

--- components/rrss_links.js
+++ components/rrss_links.js
@@ -6,13 +6,13 @@ export default function RRSSLinks() {
         <div className={styles.container}>
             <ul>
                 <li>
-                    <a href="https://github.com/vijoin"><Image src="/images/github.png" width={50} height={50} /></a>
+                    <a href="https://github.com/vijoin" target="_blank"><Image src="/images/github.png" width={50} height={50} /></a>
                 </li>
                 <li>
-                    <a href="https://linkedin.com/in/vijoin"><Image src="/images/linkedin.png" width={50} height={50} /></a>
+                    <a href="https://linkedin.com/in/vijoin" target="_blank"><Image src="/images/linkedin.png" width={50} height={50} /></a>
                 </li>
                 <li>
-                    <a href="https://twitter.com/vijoin"><Image src="/images/twitter.png" width={50} height={50} /></a>
+                    <a href="https://twitter.com/vijoin" target="_blank"><Image src="/images/twitter.png" width={50} height={50} /></a>
                 </li>
             </ul>
         </div>
